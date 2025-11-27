52b2b2c6ddf4de2e7531d58870ae446fab2f93f6
sungchul02 <robin0307choi@gmail.com>
2025-11-27 16:32:54 +0900

    prepare deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/minwooo/",  // ⭐ repo 이름 그대로 써야 함!
+});
